a4becd9bc43810bd4e53cb384a2d3b0ea84e219c
Dale-Kurt Murray <hello@dalekurtmurray.com>
2019-11-15 18:55:46 -0500

    Added lables module for IAM user tags

--- variables.tf
+++ variables.tf
@@ -1,3 +1,44 @@
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
+  default     = ""
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating any resources"
+  default     = true
+}
+
+variable "name" {
+  type        = string
+  description = "Name  (e.g. `app` or `cluster`)"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
 variable "enabled" {
   description = "Whether to create the IAM user"
   default     = "true"
