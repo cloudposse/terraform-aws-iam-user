3cac38fdf7c144c88e35486d75ab5165db293c7a
Dale-Kurt Murray <hello@dalekurtmurray.com>
2019-11-15 19:31:36 -0500

    Using terraform 0.11.0 for label module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.11/master"
   enabled    = "${var.enabled}"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
