8c2ca21ef81698bf3c0501c48e121d3137459e7c
Dale-Kurt Murray <hello@dalekurtmurray.com>
2019-11-15 19:24:50 -0500

    Removed enabled from the module label

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
-  enabled    = "${var.enabled}"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
