363cb329a715010ae81a33a20a062618e08470c6
Dale-Kurt Murray <dalekurt.murray@sema4.com>
2019-11-18 17:37:41 -0500

    Tags for AWS IAM Users (#4)
    * Added lables module for IAM user tags
    
    * Removed duplicate variable
    
    * Fixed variables
    
    * Fixed variables
    
    * Fixed variables
    
    * Removed enabled from the module label
    
    * Added enabled tot he module label
    
    * Using terraform 0.11.0 for label module
    
    * Fix modules source
    
    * Removed duplicate variable name
    
    * Removed name from lable module
    
    * Reverted changes to the module label

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.11/master"
+  enabled    = "${var.enabled}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = "${var.attributes}"
+  tags       = "${var.tags}"
+}
+
 resource "aws_iam_user" "default" {
   count = "${var.enabled == "true" ? 1 : 0}"
 
@@ -5,6 +16,7 @@ resource "aws_iam_user" "default" {
   path                 = "${var.path}"
   permissions_boundary = "${var.permissions_boundary}"
   force_destroy        = "${var.force_destroy}"
+  tags                 = "${module.label.tags}"
 }
 
 resource "aws_iam_user_login_profile" "default" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
+  default     = ""
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
 variable "enabled" {
   description = "Whether to create the IAM user"
   default     = "true"
