34eab3758f579742ceb293551498b7681aa3ea34
Chess Seventh <chess7th@pm.me>
2020-01-15 20:45:39 +0100

    Add types for all varibles for consistency.

--- variables.tf
+++ variables.tf
@@ -29,20 +29,24 @@ variable "tags" {
 }
 
 variable "enabled" {
+  type        = bool
   description = "Whether to create the IAM user"
   default     = true
 }
 
 variable "login_profile_enabled" {
+  type        = bool
   description = "Whether to create IAM user login profile"
   default     = true
 }
 
 variable "name" {
+  type        = string
   description = "Desired name for the IAM user. We recommend using email addresses."
 }
 
 variable "path" {
+  type        = string
   description = "Desired path for the IAM user"
   default     = "/"
 }
@@ -54,25 +58,30 @@ variable "groups" {
 }
 
 variable "permissions_boundary" {
+  type        = string
   description = "The ARN of the policy that is used to set the permissions boundary for the user"
   default     = ""
 }
 
 variable "force_destroy" {
+  type        = bool
   description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
   default     = false
 }
 
 variable "pgp_key" {
+  type        = string
   description = "Provide a base-64 encoded PGP public key, or a keybase username in the form `keybase:username`. Required to encrypt password."
 }
 
 variable "password_reset_required" {
+  type        = bool
   description = "Whether the user should be forced to reset the generated password on first login."
   default     = true
 }
 
 variable "password_length" {
+  type        = number
   description = "The length of the generated password"
   default     = 24
 }
