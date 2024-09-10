d4734d7ae41f067b7aac56b2d4c6b0cf5c38a74b
Florin Andrei <901867+FlorinAndrei@users.noreply.github.com>
2024-09-10 14:55:33 -0700

    allow login profile to be disabled

--- variables.tf
+++ variables.tf
@@ -36,6 +36,7 @@ variable "force_destroy" {
 variable "pgp_key" {
   type        = string
   description = "Provide a base-64 encoded PGP public key, or a keybase username in the form `keybase:username`. Required to encrypt password."
+  default     = ""
 }
 
 variable "password_reset_required" {
